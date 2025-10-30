307295cb639cf3a3550b82e48ec1d6be7b082dc8
sebastianmmoralesf <sebastianm.moralesf@gmail.com>
2025-10-29 22:33:17 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
+import os
 from project import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # En desarrollo local
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+else:
+    # En producción (Gunicorn)
+    # Gunicorn se encarga de manejar el servidor
